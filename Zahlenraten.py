33f1666479a278794eb3c96f94a35492a767beab
Deepforest233 <72318130+Deepforest233@users.noreply.github.com>
2021-01-26 14:35:17 +0100

    Add files via upload

---
+++ Zahlenraten.py
@@ -0,0 +1,40 @@
+import random
+
+gesint = random.randint(1, 100)
+
+def leicht():
+  aktiv = 0
+  while aktiv < 7:
+    inpint = input("Welche Zahl könnte gesucht sein?")
+    if int(inpint) < gesint:
+      print("Die gesuchte Zahl ist größer.")
+      aktiv = aktiv + 1
+    elif int(inpint) > gesint:
+      print("Die gesuchte Zahl ist kleiner.")
+      aktiv = aktiv + 1
+    elif int(inpint) == gesint:
+      print("Super! Du hast gewonnen!")
+      aktiv = 0
+      break
+
+def schwer():
+  aktiv = 0
+  while aktiv < 6:
+    inpint = input("Welche Zahl könnte gesucht sein?")
+    if int(inpint) < gesint:
+      print("Die gesuchte Zahl ist größer.")
+      aktiv = aktiv + 1
+    elif int(inpint) > gesint:
+      print("Die gesuchte Zahl ist kleiner.")
+      aktiv = aktiv + 1
+    elif int(inpint) == gesint:
+      print("Super! Du hast gewonnen!")
+      aktiv = 0
+      break
+gew = "nein"
+print("Hallo, ich habe mir eine Zahl ausgedacht und du musst sie erraten.\nMöchtest du den leichten Modus, oder den schweren? ")
+modus = input()
+if modus == "leicht":
+  leicht()
+else:
+  schwer()
